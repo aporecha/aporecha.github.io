53ae6750ba3b9a9c0a841eed06a197e3ee86a7d9
aporecha <40213432+aporecha@users.noreply.github.com>
2020-12-10 21:22:52 -0500

    Update chamba.js

--- chamba.js
+++ chamba.js
@@ -1,4 +1,4 @@
 //function to return output on click of button
 function alinaButton() {
-  document.getElementById("demo").innerHTML = "Hi ALINA!";
+  document.getElementById("demo").innerHTML = "Hey ALINA, you are a cool cat in the hat!";
 }
